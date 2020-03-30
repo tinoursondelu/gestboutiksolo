574b9b43a453a8bdb4c8655bb5f2518859606472
olivier <guiotolivier84120@gmail.com>
2020-03-30 14:04:25 +0200

    New test for itemStore controller

--- src/main/java/com/shop/boutik/controller/ItemStoreController.java
+++ src/main/java/com/shop/boutik/controller/ItemStoreController.java
@@ -2,10 +2,15 @@ package com.shop.boutik.controller;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,26 +22,62 @@ import com.shop.boutik.service.ItemStoreService;
 @RestController
 @RequestMapping("/itemStore")
 public class ItemStoreController {
-	
+
 	@Autowired
 	private ItemStoreService itemStoreService;
-	
+
+
+//	TODO: securiser update et delete
 	
 	@GetMapping("/")
 	public List<ItemStoreDto> listItemStore() {
-		
+
 		Collection<ItemStore> itemStore = itemStoreService.findAll();
 		for (ItemStore is : itemStore) {
 			System.out.println("id = " + is.getId());
 		}
-		
+
 		return (List<ItemStoreDto>) HelperItemStore.parseListModelToDto(itemStoreService.findAll());
 	}
-	
-	@GetMapping("{id}")
-	public ItemStoreDto plop(@PathVariable("id") Long id) {
-		
-		return HelperItemStore.parseModelToDto(itemStoreService.findById(id).get());
+
+	@GetMapping("/{itemStoreId}")
+	public ItemStoreDto plop(@PathVariable("itemStoreId") Long itemStoreId) {
+
+		ItemStoreDto itemStoreDto = new ItemStoreDto();
+
+		Optional<ItemStore> dbItemStore = itemStoreService.findById(itemStoreId);
+		if (dbItemStore.isPresent()) {
+
+			itemStoreDto = HelperItemStore.parseModelToDto(dbItemStore.get());
+		} else {
+			System.out.println("itemStore with id '" + itemStoreId + "' not found");
+		}
+		return itemStoreDto;
 	}
+
+	@PostMapping("/create")
+	public void create(@RequestBody ItemStoreDto itemStoreDto) {
+
+		HelperItemStore.create(itemStoreDto);
+	}
+
+	@PutMapping("/update")
+	public ItemStoreDto update(@RequestBody ItemStoreDto itemStoreDto) {
+
+		return HelperItemStore.parseModelToDto(HelperItemStore.update(itemStoreDto));
+	}
+
+	@DeleteMapping("/delete")
+	public void delete(@RequestBody ItemStoreDto itemStoreDto) {
+
+		HelperItemStore.delete(itemStoreDto.getId());
+	}
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/shop/boutik/model/ItemStore.java
+++ src/main/java/com/shop/boutik/model/ItemStore.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
